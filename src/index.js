edf0965379fd249977c792aeccdf2253d90c9851
Ives van Hoorne <Ives.v.h@gmail.com>
2017-11-10 00:35:53 +0100

    Cleanup

--- src/index.js
+++ src/index.js
@@ -3,8 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-console.log('Hey, this is new?');
-
-console.log('this is outdated')
-
 ReactDOM.render(<App />, document.getElementById('root'));
