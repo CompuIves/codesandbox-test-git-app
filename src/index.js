c66a4aec1da1e3a827ace6da42d3ac370c6acbbb
Ives van Hoorne <Ives.v.h@gmail.com>
2017-11-09 23:16:34 +0100

    Commit 7

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-console.log('new commit 6');
+console.log('new commit 7');
 
 ReactDOM.render(<App />, document.getElementById('root'));
