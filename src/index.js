4070dbd2b17b30cebcc74f22920b51776c8750ed
Ives van Hoorne <Ives.v.h@gmail.com>
2017-11-11 16:09:16 +0100

    Seamless commit 2

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-console.log("Seamless commit.");
+console.log("Seamless commit number 2.");
 
 ReactDOM.render(<App />, document.getElementById('root'));
