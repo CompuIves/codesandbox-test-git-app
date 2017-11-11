3a1d82070ece90af2c13cc118911246bd7fb6909
Ives van Hoorne <Ives.v.h@gmail.com>
2017-11-11 15:36:58 +0100

    Commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-console.log('Hello world');
+console.log('Hello ,hwo');
 
 ReactDOM.render(<App />, document.getElementById('root'));
