8921f6316237c60c63df49bc732968326af6db8e
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2017-11-11 15:32:09 +0100

    Remote change

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
           <h2>Welcome to CodeSandbox</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get going, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
