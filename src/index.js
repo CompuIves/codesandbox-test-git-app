28e502f19a57c58e17f83f765ab06ed0de17c71e
Ives van Hoorne <Ives.v.h@gmail.com>
2017-11-11 18:10:46 +0100

    Yes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-console.log("Seamless commit number 4");
-
 ReactDOM.render(<App />, document.getElementById('root'));
