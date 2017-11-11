85b02aeed051f46ca56d0680d234a5d345e0533c
Ives van Hoorne <Ives.v.h@gmail.com>
2017-11-11 16:34:11 +0100

    Yoo

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-console.log("Seamless commi");
+console.log("Seamless");
 
 ReactDOM.render(<App />, document.getElementById('root'));
