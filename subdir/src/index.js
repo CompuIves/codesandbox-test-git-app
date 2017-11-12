0986f4159f50bc4ecfadade65d848fac091110ac
Ives van Hoorne <Ives.v.h@gmail.com>
2017-11-12 18:59:28 +0100

    Hello

--- subdir/src/index.js
+++ subdir/src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+console.log('wtf, even the service worker works')
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
