712ae48ec893defd3cf28c4129c40c2f611e5b20
Ives van Hoorne <Ives.v.h@gmail.com>
2017-11-12 19:07:53 +0100

    Do a direct commit

--- subdir/src/index.js
+++ subdir/src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+console.log('saving the world');
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
