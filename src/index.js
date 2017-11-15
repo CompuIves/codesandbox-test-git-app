1f812f60fefa768a85c17f8252719ea4b1dcfabe
Ives van Hoorne <Ives.v.h@gmail.com>
2017-11-15 23:22:47 +0100

    Remove console.log message

--- src/index.js
+++ src/index.js
@@ -4,8 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-console.log('saving the world');
-
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
