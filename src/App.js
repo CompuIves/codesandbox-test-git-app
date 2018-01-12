092d4d789f8f30872e67da9bed044908f594ac5d
Ives van Hoorne <Ives.v.h@gmail.com>
2018-01-12 13:36:40 +0100

    Mark the redesign

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to CodeSandbox</h1>
+          <h1 className="App-title">Welcome to the redesigned CodeSandbox</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
