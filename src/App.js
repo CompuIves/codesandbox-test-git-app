9a7b264d2ae33a7d6bd273cd1c301fb2c2a2190e
Ives van Hoorne <i.vanhoorne@catawiki.nl>
2018-01-12 13:42:20 +0100

    Original

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to the new CodeSandbox</h1>
+          <h1 className="App-title">Welcome to the original CodeSandbox</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
