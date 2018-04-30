b89938f88031fbb3516df7f019482242ebacec28
Ives van Hoorne <Ives.v.h@gmail.com>
2018-04-30 10:24:45 -0700

    Pok

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-console.log("hello");
+console.log("hello kek 2");
 
 ReactDOM.render(<App />, document.getElementById("root"));
