77b78e2830e6379f9f3c6263a013792d879e22e5
Ives van Hoorne <Ives.v.h@gmail.com>
2018-04-30 10:22:10 -0700

    Niec

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-console.log("hello");
+console.log("hello kek");
 
 ReactDOM.render(<App />, document.getElementById("root"));
